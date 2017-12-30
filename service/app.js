5a72dbcb5dc2b7a254340300c4d042d006f7c692
richie-south <richard.soderman@gmail.com>
2017-12-30 17:21:07 +0100

    feat: added cors

--- service/app.js
+++ service/app.js
@@ -1,4 +1,5 @@
-const {send, createError, json} = require('micro')
+const {send} = require('micro')
+const cors = require('micro-cors')()
 const {router, post, get} = require('microrouter')
 const getPoints = require('./src/routes/get-points')
 const motifyPoints = require('./src/routes/modify-points')
@@ -6,8 +7,8 @@ const motifyPoints = require('./src/routes/modify-points')
 const notfound = (req, res) =>
   send(res, 404, 'Not found route')
 
-module.exports = router(
+module.exports = cors(router(
   post('/', motifyPoints),
   get('/', getPoints),
   get('/*', notfound)
-)
+))
